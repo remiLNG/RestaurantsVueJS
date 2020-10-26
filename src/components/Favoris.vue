c8a254eb017e79eddfeb0438efe73fd59fc9ffd0
remi <remi.longin@outlook.fr>
2020-10-26 14:27:00 +0100

    change favorite page design

--- src/components/Favoris.vue
+++ src/components/Favoris.vue
@@ -1,39 +1,42 @@
 <template>
   <div>
-    <div id="fav-img">
-      <h1>Liste des favoris </h1>
-     
-      <v-simple-table v-if="fav && fav.length>0">
-        <thead v-if="fav">
-          <tr>
-            <th>Nom</th>
-            <th>Cuisine</th>
-            <th>Detail</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(f, i) in fav" :key="i">
-            <td>{{ fav[i].name }}</td>
-            <td>{{ fav[i].cuisine }}</td>
-            <td class="info-btn">
-              <v-btn icon>
-                <router-link :to="'/detail/' + fav[i]._id"
-                  ><v-icon transparent>
-                    fas fa-info-circle
-                  </v-icon></router-link
-                >
-              </v-btn>
-            </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-      <v-btn>
-        <router-link
-          :to="{ name: 'ListeDesRestaurants', params: { fav1: fav } }"
-          >Retour</router-link
-        >
-      </v-btn>
+    <div class="toolbar">
+      <v-app-bar color="white" dense flat>
+        <v-btn>
+          <router-link
+            :to="{ name: 'ListeDesRestaurants', params: { fav1: fav } }"
+            >Retour</router-link
+          >
+        </v-btn>
+      </v-app-bar>
     </div>
+    <div id="fav-img">
+      <h1>Liste des favoris</h1>
+    </div>
+
+    <v-simple-table v-if="fav && fav.length > 0">
+      <thead v-if="fav">
+        <tr>
+          <th>Nom</th>
+          <th>Cuisine</th>
+          <th>Detail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(f, i) in fav" :key="i">
+          <td>{{ fav[i].name }}</td>
+          <td>{{ fav[i].cuisine }}</td>
+          <td class="info-btn">
+            <v-btn icon>
+              <router-link :to="'/detail/' + fav[i]._id"
+                ><v-icon transparent> fas fa-info-circle </v-icon></router-link
+              >
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+
     <v-footer color="#FAF1ED">
       <v-col class="text-center">
         <p>
